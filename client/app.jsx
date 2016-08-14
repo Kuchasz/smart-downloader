c9ac45d381ee7e855f1e9b96816d8985084ee15a
Rafau <kuchasz1@o2.pl>
2016-08-14 23:58:15 +0200

    redux added

--- client/app.jsx
+++ client/app.jsx
@@ -7,8 +7,31 @@ import * as Rx from 'rxjs/Rx';
 var io = require('socket.io-client');
 import ReactDOM from 'react-dom';
 import React from 'react';
+import {createStore, combineReducers} from 'redux';
 
 import {Incrementer} from './src/views/Incrementer.jsx';
+
+const {defaultPerson, defaultNote} = [{name: 'unnamed', age: 0}, {note: 0, subject: 'unnamed'}];
+
+let person = (state = {}, action) => {
+  switch (action.type) {
+    case 'RENAME': return {...state, name: action.name};
+    case 'REAGE': return {...state, age: action.age};
+    default: return state;
+  }
+}
+
+let noter = (state = {}, action) => {
+  switch (action.type) {
+    case 'SET_NOTE': return {...state, val: action.val};
+    case 'SET_SUBJECT': return {...state, subject: action.subject};
+    default: return state;
+  }
+}
+
+
+const reducers = combineReducers({person, noter});
+const store = createStore(reducers);
 
 // Rx.Observable.interval(100).take(100).reduce((r, t)=>r+t, 0).subscribe(x=>console.log(x));
 // var newBie = new Newbie('Mark', 23);
@@ -21,38 +44,49 @@ import {Incrementer} from './src/views/Incrementer.jsx';
 //   newBie.age(24);
 // }, 2500));
 //
+//
+// const hooks = {
+//   click: new Rx.Subject()
+// };
+//
+// Rx.Observable.merge(
+//   hooks.click
+// ).subscribe((val)=>alert(`run!!: ${val}`));
+//
+// hooks.click.next(10);
 
-const hooks = {
-  click: new Rx.Subject()
+const render = () => {
+  let state = store.getState();
+  ReactDOM.render(<Incrementer {...state}
+    rename={()=>store.dispatch({type: 'RENAME', name: 'Dory'})}
+    reage={()=>store.dispatch({type: 'REAGE', age: 22})}
+    note={()=>store.dispatch({type: 'SET_NOTE', val: 12})}
+    resubject={()=>store.dispatch({type: 'SET_SUBJECT', subject: 'Biology'})}
+    />, document.querySelector('#incrementer'));
 };
 
-Rx.Observable.merge(
-  hooks.click
-).subscribe((val)=>alert(`run!!: ${val}`));
+render();
+store.subscribe(render);
 
-hooks.click.next(10);
-
-ReactDOM.render(<Incrementer val='ziomeczek'/>, document.querySelector('#incrementer'));
-
-var map = new Map([['aa', 10],['bb', 20],['cc', 30],['dd', 40],['ee', 50]]);
-console.log(map);
-
-function* fireUp(){
-  yield 0;
-  yield 1;
-  yield 2;
-  yield 3;
-  yield 4;
-  yield 5;
-}
-
-var ioo = io('http://localhost:3000');
-ioo.on('foo', (msg)=>console.log(msg));
-ioo.on('connect', ()=>console.log('connected!'));
-
-const iterator = fireUp();
-for(let i=0; i<5; i++)setTimeout(()=>newBie.name('Tiffany ' + iterator.next().value), 1000*i);
-
+// var map = new Map([['aa', 10],['bb', 20],['cc', 30],['dd', 40],['ee', 50]]);
+// console.log(map);
+//
+// function* fireUp(){
+//   yield 0;
+//   yield 1;
+//   yield 2;
+//   yield 3;
+//   yield 4;
+//   yield 5;
+// }
+//
+// var ioo = io('http://localhost:3000');
+// ioo.on('foo', (msg)=>console.log(msg));
+// ioo.on('connect', ()=>console.log('connected!'));
+//
+// const iterator = fireUp();
+// for(let i=0; i<5; i++)setTimeout(()=>newBie.name('Tiffany ' + iterator.next().value), 1000*i);
+//
 const newBie = {
   name: ko.observable('Julia'),
   age: ko.observable(22),
@@ -69,10 +103,10 @@ const newBie = {
     ioo.emit('download-file', {url: newBie.url(), id: _progress.id})
   }
 };
-
-ioo.on('download-progress', (progress)=>{
-  const _progress = newBie.filesProgress().find(p=>p.id == progress.id);
-  _progress.progress(progress.progress);
-});
+//
+// ioo.on('download-progress', (progress)=>{
+//   const _progress = newBie.filesProgress().find(p=>p.id == progress.id);
+//   _progress.progress(progress.progress);
+// });
 
 ko.applyBindings(newBie);
--- client/src/views/Incrementer.jsx
+++ client/src/views/Incrementer.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 class Incrementer extends React.Component {
   render(){
-    return <div><h1>{this.props.val}</h1></div>;
+    return <div>
+      <h1 onClick={()=>this.props.rename()}>NAME: {this.props.person.name}</h1>
+      <h1 onClick={()=>this.props.reage()}>AGE: {this.props.person.age}</h1>
+      <h1 onClick={()=>this.props.resubject()}>SUBJECT: {this.props.noter.subject}</h1>
+      <h1 onClick={()=>this.props.note()}>NOTE: {this.props.noter.val}</h1>
+      </div>;
   }
 }
 
