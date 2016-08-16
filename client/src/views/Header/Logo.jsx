28a2f317aa1a669f70aeea47425ab4f2225e89a4
Rafau <kuchasz1@o2.pl>
2016-08-16 23:31:20 +0200

    refs test

--- client/src/views/Header/Logo.jsx
+++ client/src/views/Header/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export class Logo extends React.Component {
   render (){
     return <div className="logo">
-          <h1>SMART <sup>downloader</sup></h1>
+          <h1 ref={(element)=>console.log(element)}>SMART <sup>downloader</sup></h1>
         </div>;
   }
 }
