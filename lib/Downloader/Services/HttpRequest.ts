4bb24e4f9af2b28b5aa4b30054073a9d1e902a37
Rafal Pysz <kuchasz1@o2.pl>
2016-11-05 10:24:48 +0100

    abstract http request service, idea files removed

--- lib/Downloader/Services/HttpRequest.ts
+++ lib/Downloader/Services/HttpRequest.ts
@@ -1,17 +1,20 @@
 import {UrlNotSupportedException} from "../Exceptions/UrlNotSupportedException";
+import * as _http from 'http';
+import * as _https from 'https';
+import {ClientRequest} from "http";
 
 const serviceProviders = [
   {
     regExp: /^http:\/\/.+/,
-    service: require('http')
+    service: _http
   },
   {
     regExp: /^https:\/\/.+/,
-    service: require('https')
+    service: _https
   }];
 
-export const getDownloadService = (url) => {
+export const getDownloadService = (url): ClientRequest  => {
     const serviceProvider = serviceProviders.find(s => s.regExp.test(url));
     if(!serviceProvider) throw new UrlNotSupportedException();
-    return serviceProvider.service;
+    return serviceProvider.service.request(url);
 };
